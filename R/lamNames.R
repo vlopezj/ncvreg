6c1487b9bdc5edda39c62011b9eefc6c9048699b
Víctor López Juan <victor@lopezjuan.com>
2019-08-30 13:16:33 +0200

    Update lamNames.R

--- R/lamNames.R
+++ R/lamNames.R
@@ -1,9 +1,3 @@
 lamNames <- function(l) {
-  if (length(l) > 1) {
-    d <- ceiling(-log10(-max(diff(l))))
-    d <- min(max(d,4), 10)
-  } else {
-    d <- 4
-  }
-  formatC(l, format="f", digits=d)
+  formatC(l, format="f", digits=4)
 }
